0d32f67609900af3ae8bc16147bd74f4d42016eb
T.G <farmer1992@gmail.com>
2012-12-24 15:24:19 +0800

    fix toc file not inclued

--- wowace.pkgmeta.gen.py
+++ wowace.pkgmeta.gen.py
@@ -9,8 +9,9 @@ conf = yaml.load(f)
 f.close()
 
 ignore = set(glob('*') + glob('**/*') + glob('**/**/*')) # --b
-f = open(glob('*.toc')[0])
-need = set([l.replace('\\','/') for l in f.read().splitlines() if not l.startswith('#')])
+toc = glob('*.toc')[0]
+f = open(toc)
+need = set([l.replace('\\','/') for l in f.read().splitlines() if not l.startswith('#')] + [toc])
 conf['ignore'] = [t for t in (ignore - need) if not os.path.isdir(t)]
 
 f.close()
